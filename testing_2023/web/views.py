2fc44c3cbb3b981d49cabb019ac08b77f134bb9a
Bogomila <bkatsarska@abv.bg>
2023-02-15 17:44:47 +0200

    second commit

--- testing_2023/web/views.py
+++ testing_2023/web/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import generic as views
 
+from testing_2023.web.models import Profile
+
 
 # No need to test the code below
 class EmployeesListView(views.ListView):
-    template_name = '...'
-    model = None
+    template_name = 'profile/list.html'
+    model = Profile
